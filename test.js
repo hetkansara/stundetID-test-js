2e5c07dc6e65780c8c88f9f6009f812dcf3da66d
hetkansara <hetkansara.work@gmail.com>
2020-01-24 10:25:56 -0500

    More testcases added

--- test.js
+++ test.js
@@ -16,17 +16,23 @@ function test__checkHumbrId(valueIn, expected){
         testresult = "<span class='failed'>xxFAILEDxx</span>";
     }
     
-    let msg = "Value tested: " + valueIn + " Expected result: " + expected + " " + testresult + "<br>";
+    let msg = "Value tested: <b>" + valueIn + "</b> Expected result: <b>" + expected + "</b> Result: <b>" + result + "</b> " + testresult + "<br>";
 
     let msgBox = document.getElementById("msgBox");
     msgBox.innerHTML += msg;
 
 }
 
-test__checkHumbrId("n12345678", true);
+test__checkHumbrId("n12345678", false);
 test__checkHumbrId("N12345678", true);
 test__checkHumbrId("a12345678", false);
 test__checkHumbrId("1234", false);
 test__checkHumbrId(undefined, false);
 test__checkHumbrId(1.2, false);
 test__checkHumbrId(true, false);
+test__checkHumbrId("Hello", false);
+test__checkHumbrId("N013300091", false);
+test__checkHumbrId("N0133000", false);
+test__checkHumbrId("N01330009", true);
+test__checkHumbrId(function(){}, false);
+test__checkHumbrId(0, false);
